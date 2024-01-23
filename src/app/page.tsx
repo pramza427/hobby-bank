1ced8896c52673057aeec6fa44b386ad7b228a07
pramza427 <pramza427@gmail.com>
2024-01-23 13:50:31 -0600

    Basic stopwatch working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
 'use client'
 import Image from 'next/image';
 import { useState } from 'react';
+import { useStopwatch } from 'react-timer-hook';
 
-const hobbies = [
+var hobbies = [
     {
         title: 'Woodworking', defaultPrice: 6.5, id: 1,
         projects: [
-            { title: 'Jewely Box', price: 7, id: 12, parentID: 1, time: 650 },
-            { title: 'Ruler Stand', price: 2, id: 14, parentID: 1, time: 300 },
-            { title: 'Mask', price: 6.5, id: 15, parentID: 1, time: 8400 }
+            { title: 'Jewely Box', price: 7, id: 12, parentID: 1, time: 650, timing: false },
+            { title: 'Ruler Stand', price: 2, id: 14, parentID: 1, time: 300, timing: false },
+            { title: 'Mask', price: 6.5, id: 15, parentID: 1, time: 8400, timing: false }
         ]
     },
     {
         title: 'Quilting', defaultPrice: 8.5, id: 2,
         projects: [
             {
-                title: 'Void', price: 7, id: 16, parentID: 2, time: 650
-                
+                title: 'Void', price: 7, id: 16, parentID: 2, time: 650, timing: false
+
             },
-            { title: 'Koozies', price: 2, id: 17, parentID: 2, time: 300 },
-            { title: '3D Large', price: 6.5, id: 18, parentID: 2, time: 94300 }
+            { title: 'Koozies', price: 2, id: 17, parentID: 2, time: 300, timing: false },
+            { title: '3D Large', price: 6.5, id: 18, parentID: 2, time: 94300, timing: false }
         ]
     }
 ];
@@ -33,6 +34,11 @@ export default function Home() {
         return hobbies.find(hobbie => hobbie.id == currentHobbie)
     }
 
+    function getCurrentProject() {
+        var hobbie = getCurrentHobbie();
+        return hobbie?.projects?.find(project => project.id == currentProject)
+    }
+
     return (
         <main className="debug flex min-h-screen text-lg">
             <HobbieList currentHobbie={currentHobbie} setCurrentHobbie={setCurrentHobbie} get={getCurrentHobbie} />
@@ -44,7 +50,7 @@ export default function Home() {
 
 function HobbieButton({ currentHobbie, setCurrentHobbie, hobbie }) {
     function clickHandle() {
-        set(hobbie.id)
+        setCurrentHobbie(hobbie.id)
     }
     const totalTime = hobbie.projects.reduce((total, project) => total + project.time, 0)
     const isSelected = (currentHobbie == hobbie.id) ? "bg-blue-800" : ""
@@ -82,28 +88,19 @@ function HobbieList({currentHobbie, setCurrentHobbie, get}) {
     )
 }
 
-function convertTime(seconds) {
-    var hours = seconds / 3600
-    var minutes = seconds % 3600 / 60
-    var seconds = seconds % 60
-    return parseInt(hours) + " hrs " + parseInt(minutes) + " mins " + parseInt(seconds)
+function convertTime(days: number, hours: number, minutes: number, seconds:number) {
+    return days + " days " + hours + " hrs " + minutes + " mins " + seconds
 }
 
-function ProjectDetails({ project, timing, setTiming, setStartTime }) {
+function ProjectDetails({ project, isRunning, startTiming, stopTiming }) {
+
     function toggleExpenses() {
         setExpenses(!showExpenses)
     }
     function toggleTimer() {
-        if (!timing) {
-            const d = new Date();
-            setStartTime(d.getTime() / 1000)
-        }
-        else {
-
-        }
-        setTiming(!timing) 
+        isRunning ? stopTiming() : startTiming();
     }
-    var timerText = timing ? "Stop Timer" : "Start Timer"
+    var timerText = isRunning ? "Stop Timer" : "Start Timer"
     return (
         <div className="relative flex justify-center">
             <div className="absolute left-0 mx-3 my-2 p-2 rounded-lg text-center hover:cursor-pointer hover:bg-metal">Edit</div>
@@ -120,20 +117,20 @@ function ProjectDetails({ project, timing, setTiming, setStartTime }) {
 }
 
 function ProjectItem({ setCurrentProject, project, currentProject }) {
-    const [timing, setTiming] = useState(false);
-    const [time, setTime] = useState(project.time);
-    const [startTime, setStartTime] = useState(0);
+    const {
+        totalSeconds,
+        seconds,
+        minutes,
+        hours,
+        days,
+        isRunning,
+        start,
+        pause,
+        reset,
+    } = useStopwatch({ autoStart: false, stopwatchOffset: 36000 });
     const [openExpenses, setOpenExpenses] = useState(0);
-
-    const interval = setInterval(increment, 1000);
-    function increment() {
-        if (timing) {
-            console.log(project.title, time, timing)
-            setTime(time + 1);
-        }
-        
-    }
-
+    
+    
     function setProject() {
         setCurrentProject(project.id)
     }
@@ -142,23 +139,23 @@ function ProjectItem({ setCurrentProject, project, currentProject }) {
     let lowerSection;
     let timingClass = "";
     if (isCurrentProject) {
-        lowerSection = <ProjectDetails project={project} timing={timing} setTiming={setTiming} setStartTime={setStartTime} />
+        lowerSection = <ProjectDetails project={project} isRunning={isRunning} startTiming={start} stopTiming={pause}/>
         selectedClass = "bg-midnight "
     }
     else {
         lowerSection = <div></div>
         selectedClass = "hover:bg-metal "
     }
-    if (timing) { timingClass = " timing"}
+    { isRunning ? timingClass = " timing " : ""}
 
     return (
-        <div className={"p-2 " + selectedClass + timingClass}>
-            <div className="grid grid-cols-4 hover:cursor-pointer"
-                onClick={setProject}>
+        <div className={"p-2 hover:cursor-pointer " + selectedClass + timingClass}
+            onClick={setProject}>
+            <div className="grid grid-cols-4">
                 <div className="">{project.title}</div>
                 <div className="text-right">{parseFloat(project.price).toFixed(2)}</div>
-                <div className="text-right">{convertTime(time)}</div>
-                <div className="text-right">{"$" + (time / 3600 * project.price).toFixed(2)}</div>
+                <div className="text-right">{convertTime(days, hours, minutes, seconds)}</div>
+                <div className="text-right">{"$" + (totalSeconds / 3600 * project.price).toFixed(2)}</div>
             </div>
             {lowerSection}
         </div>
